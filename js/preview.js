8598d4f680441ae02409490070a7c73ee039f12a
EOnyenezido <sleonvictor@gmail.com>
2015-04-20 08:28:28 +0100

    added some tooltips

--- js/preview.js
+++ js/preview.js
@@ -3,3 +3,4 @@ $('#fullScreenPreview').empty();
 $('#fullScreenPreview').append("<style>" + cssState + "</style>");
 $('#fullScreenPreview').append(htmlState);
 $('#fullScreenPreview').append("<script>" + jsState + "</script>");
+Materialize.toast('Use browser back button to return!', 2000)
