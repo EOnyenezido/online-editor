04abbfed5447780d49502e49eb62b49a7a51fa97
EOnyenezido <sleonvictor@gmail.com>
2015-04-20 11:47:10 +0100

    All E2E tests passed

--- js/preview.js
+++ js/preview.js
@@ -3,4 +3,3 @@ $('#fullScreenPreview').empty();
 $('#fullScreenPreview').append("<style>" + cssState + "</style>");
 $('#fullScreenPreview').append(htmlState);
 $('#fullScreenPreview').append("<script>" + jsState + "</script>");
-Materialize.toast('Use browser back button to return!', 2000)
